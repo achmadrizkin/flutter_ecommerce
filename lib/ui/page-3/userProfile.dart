0092705dfbf9bcd58351e5fbdcd0f1e5c43506ac
achmadrizkin <arizki.nf02@gmail.com>
2021-02-22 12:58:49 +0700

    update userProfile

--- lib/ui/page-3/userProfile.dart
+++ lib/ui/page-3/userProfile.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommers/ui/page-3/addProduct.dart';
+import 'package:flutter_ecommers/ui/page-3/addProductAdmin.dart';
+import 'package:flutter_ecommers/ui/page-3/myproduct.dart';
+import 'package:flutter_ecommers/ui/page-3/transactionHistory.dart';
 
 class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(10.0),
@@ -11,7 +16,7 @@ class UserProfile extends StatelessWidget {
             children: [
               Container(
                 width: double.infinity,
-                height: MediaQuery.of(context).size.height / 5,
+                height: MediaQuery.of(context).size.height / 3,
                 decoration: BoxDecoration(
                     color: Color(0xFF016DF7),
                     borderRadius: BorderRadius.circular(20.0)),
@@ -27,7 +32,7 @@ class UserProfile extends StatelessWidget {
                       ],
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 10.0, top: 10.0),
+                      padding: const EdgeInsets.only(left: 10.0, top: 20.0),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -48,27 +53,74 @@ class UserProfile extends StatelessWidget {
                   ],
                 ),
               ),
-              SizedBox(height: 10.0),
-              Card(
-                child: ListTile(
-                    title: Text('Add Product',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'PoppinsBold',
-                            fontSize: 18)),
-                    leading: Icon(Icons.add_shopping_cart,
-                        color: Colors.black, size: 24)),
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => AddProduct()));
+                },
+                child: Card(
+                  child: ListTile(
+                      title: Text('Add Product',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'PoppinsBold',
+                              fontSize: 18)),
+                      leading: Icon(Icons.add_shopping_cart,
+                          color: Colors.black, size: 24)),
+                ),
               ),
-              SizedBox(height: 10.0),
-              Card(
-                child: ListTile(
-                    title: Text('Add Product',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'PoppinsBold',
-                            fontSize: 18)),
-                    leading: Icon(Icons.add_shopping_cart,
-                        color: Colors.black, size: 24)),
+
+              //
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => AddProductAdmin()));
+                },
+                child: Card(
+                  child: ListTile(
+                      title: Text('Add Product(Admin Only)',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'PoppinsBold',
+                              fontSize: 18)),
+                      leading: Icon(Icons.add_shopping_cart,
+                          color: Colors.black, size: 24)),
+                ),
+              ),
+
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => TransactionHistory()));
+                },
+                child: Card(
+                  child: ListTile(
+                      title: Text('Transaction History',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'PoppinsBold',
+                              fontSize: 18)),
+                      leading:
+                          Icon(Icons.history, color: Colors.black, size: 24)),
+                ),
+              ),
+
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => MyProduct()));
+                },
+                child: Card(
+                  child: ListTile(
+                      title: Text('My Product',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'PoppinsBold',
+                              fontSize: 18)),
+                      leading: Icon(Icons.storage_rounded,
+                          color: Colors.black, size: 24)),
+                ),
               ),
             ],
           ),
