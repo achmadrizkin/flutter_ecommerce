d191db3bea38e1ddeb5bbba61e7bece695572833
achmadrizkin <arizki.nf02@gmail.com>
2021-02-22 18:34:20 +0700

    update searchDetails

--- lib/ui/searchDelegate/searchDetails.dart
+++ lib/ui/searchDelegate/searchDetails.dart
@@ -15,9 +15,11 @@ class SearchDetails extends StatelessWidget {
       this.price,
       this.imageUrl,
       this.productCondition});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
           elevation: 0.0,
           backgroundColor: Colors.white,
@@ -33,20 +35,108 @@ class SearchDetails extends StatelessWidget {
                   fontFamily: 'PoppinsBold'))),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.network(
-                imageUrl,
-                fit: BoxFit.cover,
-                height: 250,
-              ),
-              Text(productName),
-              Text(stock),
-              Text(price),
-              Text(imageUrl),
-              Text(details),
-              Text(productCondition ?? 0),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Image.network(
+                    imageUrl,
+                    fit: BoxFit.cover,
+                    height: 250,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Text(productName,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'PoppinsBold',
+                          fontSize: 20.0)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                                text: 'Stock: ' + stock + '  •  ',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontFamily: 'PoppinsReg')),
+                            WidgetSpan(
+                                child: Icon(Icons.star_sharp,
+                                    color: Colors.yellowAccent)),
+                            TextSpan(
+                                text: ' 4.9' + '  •  ',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontFamily: 'PoppinsReg')),
+                            TextSpan(
+                                text: 'Condition: ' + productCondition,
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontFamily: 'PoppinsReg')),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Text('€' + price,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'PoppinsBold',
+                          fontSize: 26.0)),
+                ),
+                SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Divider(
+                    thickness: 1.0,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Text('Details',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontFamily: 'PoppinsReg')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Divider(
+                    thickness: 1.0,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Text(details,
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black,
+                          fontFamily: 'PoppinsReg')),
+                ),
+              ],
+            ),
           ),
         ),
       ),
