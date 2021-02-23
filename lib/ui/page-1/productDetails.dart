1c89c97f2472e93eabe9c401d5834c195cf0432d
achmadrizkin <arizki.nf02@gmail.com>
2021-02-23 12:48:58 +0700

    update product details

--- lib/ui/page-1/productDetails.dart
+++ lib/ui/page-1/productDetails.dart
@@ -1,29 +1,183 @@
 import 'package:flutter/material.dart';
 
 class ProductDetails extends StatelessWidget {
-  String nameProduct;
-  String emailsProduct;
-  ProductDetails({this.nameProduct, this.emailsProduct});
+  String productName;
+  String stock;
+  String details;
+  String price;
+  String imageUrl;
+  String productCondition;
+
+  ProductDetails(
+      {this.productName,
+      this.stock,
+      this.details,
+      this.price,
+      this.imageUrl,
+      this.productCondition});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amberAccent[200],
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        elevation: 0.0,
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Text(
-          nameProduct,
-          style: TextStyle(color: Colors.black),
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: Text(productName,
+              style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontFamily: 'PoppinsBold'))),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Image.network(
+                    imageUrl,
+                    fit: BoxFit.cover,
+                    height: 250,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Text(productName,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'PoppinsBold',
+                          fontSize: 20.0)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                                text: 'Stock: ' + stock + '  •  ',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontFamily: 'PoppinsReg')),
+                            WidgetSpan(
+                                child: Icon(Icons.star_sharp,
+                                    color: Colors.yellowAccent)),
+                            TextSpan(
+                                text: ' 4.9' + '  •  ',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontFamily: 'PoppinsReg')),
+                            TextSpan(
+                                text: 'Condition: ' + productCondition,
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontFamily: 'PoppinsReg')),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Text('€' + price,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontFamily: 'PoppinsBold',
+                          fontSize: 26.0)),
+                ),
+                SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Divider(
+                    thickness: 1.0,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Text('Details',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontFamily: 'PoppinsReg')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Divider(
+                    thickness: 1.0,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: Text(details,
+                      maxLines: 30,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black,
+                          fontFamily: 'PoppinsReg')),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    FlatButton(
+                      height: 50,
+                      minWidth: MediaQuery.of(context).size.width / 2.3,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      color: Colors.deepOrange,
+                      onPressed: () {},
+                      child: Text("+ Add to cart",
+                          style: TextStyle(
+                              fontFamily: 'PoppinsBold',
+                              color: Colors.white,
+                              fontSize: 16)),
+                    ),
+                    FlatButton(
+                      height: 50,
+                      minWidth: MediaQuery.of(context).size.width / 2.3,
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: Colors.deepOrange,
+                              width: 2,
+                              style: BorderStyle.solid),
+                          borderRadius: BorderRadius.circular(10.0)),
+                      color: Colors.white,
+                      onPressed: () {},
+                      child: Text("Buy Products",
+                          style: TextStyle(
+                              fontFamily: 'PoppinsBold',
+                              color: Colors.deepOrange,
+                              fontSize: 16)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-      ),
-      body: Column(
-        children: [Text(nameProduct), Text(emailsProduct)],
       ),
     );
   }
